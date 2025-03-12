61cda0b3c014d7530d4c2d48386a4883891af0bb
Domino-111 <dom.walsh@outlook.com>
2025-03-12 14:31:03 +1100

    Week6

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public List<Question> questions;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    [ContextMenu("Save Game")]
+    public void SaveGame()
+    {
+        SavedData savedData = new SavedData();
+
+        foreach (Question q in questions)
+        {
+            savedData.questions.Add(q);
+        }
+
+        JSON_Manager.SaveToJson(savedData);
+    }
+
+    [ContextMenu("Load Game")]
+    public void LoadGame()
+    {
+        SavedData newData = JSON_Manager.LoadFromJson();
+        questions.Clear();
+        foreach (Question q in newData.questions)
+        {
+            questions.Add(q);
+        }
+    }
+}
+
+[System.Serializable]
+public class Question
+{
+    public string question;
+
+    public List<string> responses = new List<string>();
+}
---
+++ Assets/Scripts/JSON_Manager.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public static class JSON_Manager
+{
+    const string SaveDirectory = "/SavedData/";
+    const string FileName = "requested template";
+
+    public static void SaveToJson(SavedData data)
+    {
+        string dir = Application.persistentDataPath + SaveDirectory;
+
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+
+        string json = JsonUtility.ToJson(data, true);
+        Debug.Log(json);
+
+        File.WriteAllText(dir + FileName, json);
+
+        GUIUtility.systemCopyBuffer = dir;
+    }
+
+    public static SavedData LoadFromJson()
+    {
+        string dir = Application.persistentDataPath + SaveDirectory + FileName;
+
+        if (File.Exists(dir))
+        {
+            string json = File.ReadAllText(dir);
+            return JsonUtility.FromJson<SavedData>(json);
+        }
+
+        else
+        {
+            Debug.Log("Load failed. No file found!"); //Did you save the file first?
+            return null;
+        }
+    }
+}
+
+public class SavedData
+{
+    public List<Question> questions = new List<Question>();
+
+    //public string name = "Indigo";
+    //public int age = 152;
+    //public Vector3 position = new Vector3(0, 50, -72);
+    //public bool haveGun = false;
+    //public List<string> children = new List<string> { "Gold", "Silver", "Bronze" };
+}
